0c965547cf8fc3c0355a68d352b83030f0cfe11b
ProgrammingPoet <muhammadrayyanmazhariqbal@gmail.com>
2026-01-22 00:45:19 +0400

    .

--- GravitySimulator/Main.cpp
+++ GravitySimulator/Main.cpp
@@ -1,14 +1,25 @@
 #include "raylib.h"
 #include <vector>
 #include <iostream>
+#include <chrono>
+#include <cmath> 
+
+
+int NumberOfMasses = 0;
+long double G = 6.6743e-11;
 
 struct Particle {
-	float Mass;
+	int Index;
+	long long int Mass;
 	float PositionX;
 	float PositionY;
 	float Radius;
 	float VelocityX;
 	float VelocityY;
+	float AccelerationX;
+	float AccelerationY;
+	float ForceX;
+	float ForceY;
 };
 
 std::vector<Particle> ParticleVector;
@@ -19,13 +30,43 @@ float StartPositionX;
 float StartPositionY;
 float EndPositionX;
 float EndPositionY;
+float Distance;
+float Angle;
+float DeltaX;
+float DeltaY;
+float UX;
+float UY; 
+float VX;
+float VY;
+bool Paused;
+long long int Mass = 9223372036854775807;
+
+
 
 
 int main() {
-	InitWindow(600, 800, "Gravity Sim");
-	SetTargetFPS(60);
+	InitWindow(1280, 720, "Gravity Sim");
 
+	//SetTargetFPS(60);
+	auto CurrentTime = std::chrono::steady_clock::now();
+
+	
 	while (WindowShouldClose() != true) {
+
+		if (IsKeyPressed(KEY_SPACE)) {
+			std::cout << "Simulation Is Currently Paused" << std::endl;
+			std::cout << "Enter Mass " << std::endl;
+			std::cin >> Mass;
+
+		}
+
+		auto PreviousTime = CurrentTime;
+		CurrentTime = std::chrono::steady_clock::now();
+		auto DeltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(CurrentTime - PreviousTime);
+		double TimePassed = DeltaTime.count() / 1000.0; //seconds
+
+
+
 		BeginDrawing();
 		ClearBackground(BLACK);
 		DrawFPS(20, 20);
@@ -35,7 +76,6 @@ int main() {
 			NeedVelocity = true;
 			StartPositionX = CursorPosition.x;
 			StartPositionY = CursorPosition.y;
-			std::cout << CursorPosition.x << ' ' << CursorPosition.y << std::endl;
 		}
 
 		if (IsMouseButtonDown(MOUSE_LEFT_BUTTON) && NeedVelocity) {
@@ -45,21 +85,68 @@ int main() {
 		}
 
 		if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON) && NeedVelocity) {
-			ParticleVector.push_back(Particle{ 20, StartPositionX, StartPositionY, 5, (EndPositionX - StartPositionX), (EndPositionY - StartPositionY) });
+			ParticleVector.push_back(Particle{ NumberOfMasses, Mass, StartPositionX, StartPositionY, 5, (EndPositionX - StartPositionX), (EndPositionY - StartPositionY), 0, 0, 0, 0});
+			NumberOfMasses++;
 			NeedVelocity = false;
 		}
 
-		for (int i = 0; i < ParticleVector.size(); i++) {
-			ParticleVector[i].PositionX += ParticleVector[i].VelocityX;
-			ParticleVector[i].PositionY += ParticleVector[i].VelocityY;
-		}
-
-		for (int i = 0; i < ParticleVector.size(); i++) {
-			DrawCircle(ParticleVector[i].PositionX, ParticleVector[i].PositionY, ParticleVector[i].Radius, BLUE);
-		}
-
-
 		
+
+		for (int i = 0; i < ParticleVector.size(); i++) {
+			ParticleVector[i].ForceX = 0;
+			ParticleVector[i].ForceY = 0;
+			for (int j = 0; j < ParticleVector.size(); j++) {
+				if (i == j) continue;
+				Distance = std::sqrt(std::pow((ParticleVector[i].PositionX - ParticleVector[j].PositionX), 2) + std::pow((ParticleVector[i].PositionY - ParticleVector[j].PositionY), 2));
+				DeltaX = ParticleVector[j].PositionX - ParticleVector[i].PositionX;
+				DeltaY = ParticleVector[j].PositionY - ParticleVector[i].PositionY;
+
+				//Angle = std::acos(ParticleVector[i].PositionX - ParticleVector[j].PositionX) / Distance;
+				ParticleVector[i].ForceX = ((G * ParticleVector[i].Mass * ParticleVector[j].Mass) / std::pow((Distance), 2)) * (DeltaX / Distance) + ParticleVector[i].ForceX;
+				ParticleVector[i].ForceY = ((G * ParticleVector[i].Mass * ParticleVector[j].Mass) / std::pow((Distance), 2)) * (DeltaY / Distance) + ParticleVector[i].ForceY;
+				ParticleVector[i].AccelerationX = ParticleVector[i].ForceX / ParticleVector[i].Mass;
+				ParticleVector[i].AccelerationY = ParticleVector[i].ForceY / ParticleVector[i].Mass;
+			}
+		}
+
+		for (int i = 0; i < ParticleVector.size(); i++) {
+
+			UX = ParticleVector[i].VelocityX;
+			UY = ParticleVector[i].VelocityY;
+
+			VX = ParticleVector[i].VelocityX + ParticleVector[i].AccelerationX * TimePassed;
+			VY = ParticleVector[i].VelocityY + ParticleVector[i].AccelerationY * TimePassed;
+
+			if (UX > 299792458) {
+				UX = 299792458;
+			}
+
+			if (UY > 299792458) {
+				UY = 299792458;
+			}
+
+			if (VX > 299792458) {
+				VX = 299792458;
+			}
+
+			if (VY > 299792458) {
+				VY = 299792458;
+			}
+
+			ParticleVector[i].PositionX += (UX + ((UX + VX)) / 2) * TimePassed;
+			ParticleVector[i].PositionY += (UY + ((UY + VY)) / 2) * TimePassed;
+			ParticleVector[i].VelocityX = VX;
+			ParticleVector[i].VelocityY = VY;
+			
+			
+		}
+
+		for (int i = 0; i < ParticleVector.size(); i++) {
+			DrawCircle(ParticleVector[i].PositionX, ParticleVector[i].PositionY, ParticleVector[i].Radius, WHITE);
+		}
+
+
+		//std::cout << NumberOfMasses << std::endl;
 		EndDrawing();
 	}
 
