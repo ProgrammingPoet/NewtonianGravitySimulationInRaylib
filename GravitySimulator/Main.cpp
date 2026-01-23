46e1f26721547669f39f35464d3a1a7d821cf715
ProgrammingPoet <muhammadrayyanmazhariqbal@gmail.com>
2026-01-24 00:23:37 +0400

    .

--- GravitySimulator/Main.cpp
+++ GravitySimulator/Main.cpp
@@ -1,46 +1,55 @@
 #include "raylib.h"
+//#define RAYGUI_IMPLEMENTATION
+//#include "raygui.h"
 #include <vector>
 #include <iostream>
 #include <chrono>
 #include <cmath> 
 
 
-int NumberOfMasses = 0;
 long double G = 6.6743e-11;
 
 struct Particle {
-	int Index;
-	long long int Mass;
-	float PositionX;
-	float PositionY;
-	float Radius;
-	float VelocityX;
-	float VelocityY;
-	float AccelerationX;
-	float AccelerationY;
-	float ForceX;
-	float ForceY;
+	long double Mass;
+	long double PositionX;
+	long double PositionY;
+	long double Radius;
+	long double VelocityX;
+	long double VelocityY;
+	long double AccelerationX;
+	long double AccelerationY;
+	long double ForceX;
+	long double ForceY;
 };
 
 std::vector<Particle> ParticleVector;
 Vector2 CursorPosition;
 
 bool NeedVelocity;
-float StartPositionX;
-float StartPositionY;
-float EndPositionX;
-float EndPositionY;
-float Distance;
-float Angle;
-float DeltaX;
-float DeltaY;
-float UX;
-float UY; 
-float VX;
-float VY;
+long double StartPositionX;
+long double StartPositionY;
+long double EndPositionX;
+long double EndPositionY;
+long double Distance;
+long double Angle;
+long double DeltaX;
+long double DeltaY;
+long double UX;
+long double UY;
+long double VX;
+long double VY;
 bool Paused;
-long long int Mass = 9223372036854775807;
+//long long int Mass = 9223372036854775807;
+long double Mass = 2e30;
+long double TotalMass;
+long double Radius;
+long double PositionX;
+long double PositionY;
 
+
+long double LengthScale = 1e9; 
+long double MassScale = 1e20; 
+long double TimeScale = 2.1e7;
 
 
 
@@ -62,8 +71,8 @@ int main() {
 
 		auto PreviousTime = CurrentTime;
 		CurrentTime = std::chrono::steady_clock::now();
-		auto DeltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(CurrentTime - PreviousTime);
-		double TimePassed = DeltaTime.count() / 1000.0; //seconds
+		auto DeltaTime = std::chrono::duration_cast<std::chrono::nanoseconds>(CurrentTime - PreviousTime);
+		long double TimePassed = (DeltaTime.count() / 1e9) * TimeScale;
 
 
 
@@ -74,20 +83,45 @@ int main() {
 		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
 			CursorPosition = GetMousePosition();
 			NeedVelocity = true;
-			StartPositionX = CursorPosition.x;
-			StartPositionY = CursorPosition.y;
+			StartPositionX = CursorPosition.x * LengthScale;
+			StartPositionY = CursorPosition.y * LengthScale;
 		}
 
 		if (IsMouseButtonDown(MOUSE_LEFT_BUTTON) && NeedVelocity) {
 			CursorPosition = GetMousePosition();
-			EndPositionX = CursorPosition.x;
-			EndPositionY = CursorPosition.y;
+			EndPositionX = CursorPosition.x * LengthScale;
+			EndPositionY = CursorPosition.y * LengthScale;
 		}
 
 		if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON) && NeedVelocity) {
-			ParticleVector.push_back(Particle{ NumberOfMasses, Mass, StartPositionX, StartPositionY, 5, (EndPositionX - StartPositionX), (EndPositionY - StartPositionY), 0, 0, 0, 0});
-			NumberOfMasses++;
+			ParticleVector.push_back(Particle{ Mass, StartPositionX, StartPositionY, (25 * LengthScale), (EndPositionX - StartPositionX) / TimePassed, (EndPositionY - StartPositionY) / TimePassed, 0, 0, 0, 0});
 			NeedVelocity = false;
+		}
+
+
+
+		for (int i = 0; i < ParticleVector.size(); i++) {
+			for (int j = 0; j < ParticleVector.size(); j++) {
+				if (i == j) continue;
+				Distance = std::sqrt(std::pow((ParticleVector[i].PositionX - ParticleVector[j].PositionX), 2) + std::pow((ParticleVector[i].PositionY - ParticleVector[j].PositionY), 2));
+				if (Distance < (ParticleVector[i].Radius + ParticleVector[j].Radius) / 2) {
+					TotalMass = ParticleVector[i].Mass + ParticleVector[j].Mass;
+					Radius = std::sqrt(((PI * ParticleVector[i].Radius * ParticleVector[i].Radius) + (PI * ParticleVector[j].Radius * ParticleVector[j].Radius)) / PI);
+					PositionX = (ParticleVector[i].PositionX + ParticleVector[j].PositionX) / 2;
+					PositionY = (ParticleVector[i].PositionY + ParticleVector[j].PositionY) / 2;
+					ParticleVector.push_back(Particle{ TotalMass, PositionX, PositionY, Radius, 0, 0, 0, 0, 0, 0 });
+					if (i > j) {
+						ParticleVector.erase(ParticleVector.begin() + i);
+						ParticleVector.erase(ParticleVector.begin() + j);
+						
+					}
+					if (j > i) {
+						ParticleVector.erase(ParticleVector.begin() + j);
+						ParticleVector.erase(ParticleVector.begin() + i);
+						
+					}
+				}
+			}
 		}
 
 		
@@ -98,14 +132,19 @@ int main() {
 			for (int j = 0; j < ParticleVector.size(); j++) {
 				if (i == j) continue;
 				Distance = std::sqrt(std::pow((ParticleVector[i].PositionX - ParticleVector[j].PositionX), 2) + std::pow((ParticleVector[i].PositionY - ParticleVector[j].PositionY), 2));
+
+
 				DeltaX = ParticleVector[j].PositionX - ParticleVector[i].PositionX;
 				DeltaY = ParticleVector[j].PositionY - ParticleVector[i].PositionY;
 
 				//Angle = std::acos(ParticleVector[i].PositionX - ParticleVector[j].PositionX) / Distance;
 				ParticleVector[i].ForceX = ((G * ParticleVector[i].Mass * ParticleVector[j].Mass) / std::pow((Distance), 2)) * (DeltaX / Distance) + ParticleVector[i].ForceX;
 				ParticleVector[i].ForceY = ((G * ParticleVector[i].Mass * ParticleVector[j].Mass) / std::pow((Distance), 2)) * (DeltaY / Distance) + ParticleVector[i].ForceY;
+
+
 				ParticleVector[i].AccelerationX = ParticleVector[i].ForceX / ParticleVector[i].Mass;
 				ParticleVector[i].AccelerationY = ParticleVector[i].ForceY / ParticleVector[i].Mass;
+				
 			}
 		}
 
@@ -142,7 +181,7 @@ int main() {
 		}
 
 		for (int i = 0; i < ParticleVector.size(); i++) {
-			DrawCircle(ParticleVector[i].PositionX, ParticleVector[i].PositionY, ParticleVector[i].Radius, WHITE);
+			DrawCircle((ParticleVector[i].PositionX / LengthScale), (ParticleVector[i].PositionY / LengthScale), (ParticleVector[i].Radius / LengthScale), WHITE);
 		}
 
 
